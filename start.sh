0a414561c3fa4b88f3c146fb1cafc96f4db2fdb4
Jon Gjengset <jon@thesquareplanet.com>
2017-05-22 16:48:34 -0400

    Always re-build (use rustup override)

--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 rustup update nightly
-if ! test -e target/debug/sunrise; then
-	cargo b --bin sunrise
-fi
-exec target/debug/sunrise "$(rustc +nightly --version)" "$(cargo +nightly --version)"
+exec cargo run --bin sunrise -- "$(rustc +nightly --version)" "$(cargo +nightly --version)"
